d770be1cd4d8138b7c87029cb8585cc268cb758e
Kirill <125193708+xaletch@users.noreply.github.com>
2023-12-22 17:16:45 +0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -109,9 +109,9 @@ closeMenu.addEventListener('click', () => {
 })
 
 // СЛАЙДЕР
-let slider = document.querySelector('.basic-services .wrapper');
-let items = document.querySelectorAll('.basic-services-card');
-let dots = document.querySelectorAll('.dots-item');
+const slider = document.querySelector('.basic-services .wrapper');
+const items = document.querySelectorAll('.basic-services-card');
+const dots = document.querySelectorAll('.dots-item');
 
 let cardWidth = 240;
 let spacing = 16;
@@ -155,36 +155,41 @@ function drag(event) {
     }
 }
 
-function reloadSlider(){
+function reloadSlider() {
     let position = -(cardWidth + spacing) * active;
-    slider.style.left = position + 'px';
+
+    slider.style.transform = `translateX(${position}px)`;
 
     let lastActiveDot = document.querySelector('.dots-item_active');
-    lastActiveDot.classList.remove('dots-item_active');
+    if (lastActiveDot) {
+        lastActiveDot.classList.remove('dots-item_active');
+    }
     dots[active].classList.add('dots-item_active');
 }
 
-function dragEnd() {
-    cancelAnimationFrame(animationID);
-    isDragging = false;
-    let movedBy = currentTranslate - prevTranslate;
-    if (movedBy < -100 && active < lengthItems) {
-        active++;
-    } else if (movedBy > 100 && active > 0) {
-        active--;
+
+function drag(event) {
+    if (!isDragging) return;
+
+    let currentPosition = event.type.includes('mouse') ? event.clientX : event.touches[0].clientX;
+    let currentPosition2 = currentPosition - startPos;
+    currentTranslate = prevTranslate + currentPosition2;
+
+    if (!animationID) {
+        animationID = requestAnimationFrame(animation);
     }
-
-    if (currentTranslate < -2290) {
-        active = 0;
-    }
-
-    currentTranslate = -(cardWidth + spacing) * active;
-    prevTranslate = currentTranslate;
-
-    slider.style.cursor = 'grab';
-
-    reloadSlider();
 }
+
+function animation() {
+    slider.style.transform = `translateX(${currentTranslate}px)`;
+    if (isDragging) {
+        requestAnimationFrame(animation);
+    }
+}
+
+window.onresize = debounce(function() {
+    reloadSlider();
+}, 250);
 
 dots.forEach((li, key) => {
     li.addEventListener('click', ()=>{
@@ -196,6 +201,22 @@ dots.forEach((li, key) => {
 window.onresize = function() {
     reloadSlider();
 };
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
 
 // Услуги по отраслям
 const servicesItem = document.querySelectorAll('.category-menu_list .item');
