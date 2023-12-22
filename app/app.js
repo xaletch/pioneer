6d6e80e55fa64cf81fc6bd2434c7e3f5ff15538e
Kirill <125193708+xaletch@users.noreply.github.com>
2023-12-23 02:03:40 +0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -265,24 +265,3 @@ servicesItem.forEach((item, index) => {
         contentCategoryHead.textContent = categoryName;
     });
 });
-
-// АККОРДЕОН
-const accordionItem = document.querySelectorAll('.questions .questions_accordion .card');
-
-accordionItem.forEach((item) => {
-    let question = item.querySelector('.questions_accordion .card .name');
-
-    question.addEventListener('click', () => {
-        item.classList.toggle('active');
-
-        let description = item.querySelector('.questions_accordion .card .description');
-
-        if (item.classList.contains('active')) {
-            description.style.height = `${description.scrollHeight}px`;
-            description.style.paddingTop = '16px';
-        } else {
-            description.style.height = '0';
-            description.style.paddingTop = '0';
-        }
-    });
-});
