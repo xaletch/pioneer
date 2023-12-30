9859ec2eafb8c3d5f37307f9fc74dcd2806fcd84
Kirill <125193708+xaletch@users.noreply.github.com>
2023-12-30 18:55:00 +0300

    Delete app directory

--- app/components/fileOpen.js
+++
@@ -1,9 +0,0 @@
-function openFileDialog() {
-    const fileInput = document.getElementById('resume');
-    fileInput.click();
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const inputWrapper = document.querySelector('.input-wrapper');
-    inputWrapper.addEventListener('click', openFileDialog);
-});
